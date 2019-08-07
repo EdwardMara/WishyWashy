c59ba08c69fa29e4423288e8682bb3651151fde1
Edward Mara <e.james.mara@gmail.com>
2019-08-07 14:14:49 -0700

    Updates

---
+++ precache-manifest.6fa370f9bb32e450a6e0ad1872fd5b80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5261c008a6c5aa7afc2e540b4e39e9d3",
+    "url": "/wishywashy/index.html"
+  },
+  {
+    "revision": "b40115dc5bcf9610e98e",
+    "url": "/wishywashy/static/css/main.0146b6f9.chunk.css"
+  },
+  {
+    "revision": "9372414360f95b384bc2",
+    "url": "/wishywashy/static/js/2.8136d07b.chunk.js"
+  },
+  {
+    "revision": "b40115dc5bcf9610e98e",
+    "url": "/wishywashy/static/js/main.7976d7ee.chunk.js"
+  },
+  {
+    "revision": "5e9bc245355c617fd005",
+    "url": "/wishywashy/static/js/runtime~main.d917a420.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wishywashy/static/media/logo.5d5d9eef.svg"
+  }
+]);
