465fd4d5617c93f3519ee1f3fbff4bc0d7df7fc8
Edward Mara <e.james.mara@gmail.com>
2019-08-09 13:51:38 -0700

    homename

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
       <div>
         <Provider store={store}>
           <Router
-        
+          basename="/WishyWashy"
           >
             <div className="App">
               <LoginNavbar />
