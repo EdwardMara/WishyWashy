e9c35845e99bafe573aed4c80676fadb83d48f66
Edward Mara <e.james.mara@gmail.com>
2019-08-11 15:55:07 -0700

    add /core to material imports to make them work

--- src/components/LoginManager.js
+++ src/components/LoginManager.js
@@ -5,9 +5,9 @@ import { connect } from "react-redux";
 import { loginManager } from "../actions/authActions";
 import TextFieldGroup from "./TextFieldGroup";
 import Link from 'react-router/lib/Link';
-import { withStyles, createStyleSheet } from 'material-ui/styles';
+// import { withStyles, createStyleSheet } from 'material-ui/styles';
 // import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
+import Button from 'material-ui/core/Button';
 // import muiLogo from 'docs/src/assets/images/material-ui-logo.svg';
 
 class LoginManager extends Component {
