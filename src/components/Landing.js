d14b359b951a94bda204113d9eb8674ad3133f23
Edward Mara <e.james.mara@gmail.com>
2019-09-16 14:44:40 -0700

    BOH images and spanned carousel

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
-import logo from '../img/wishywashylogo.png'
+import logo from '../img/wishywashylogo.png';
+import dishpit from '../img/dishpit.jpeg';
+import prep from '../img/potatos.jpeg';
+import glasses from '../img/glasses.jpeg';
+import "../reset.css";
+import { white } from 'material-ui/styles/colors';
+
+
+const carStyle = {
+  width: '100%',
+  margin: 0,
+  padding: 0,
+};
+
+const carouselText = {
+  color: 'white',
+  background: `rgba(${0}, ${0}, ${0}, ${.5})`,
+  padding: '10px',
+  marginBottom: '40%'
+};
 
 class Landing extends Component {
   componentDidMount() {
@@ -10,8 +29,8 @@ class Landing extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <div>
+        <div style={carStyle} id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
           <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -19,23 +38,23 @@ class Landing extends Component {
           </ol>
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img class="d-block w-100" src={logo} alt="First slide" />
-              <div class="carousel-caption d-none d-md-block">
-                <h5>...</h5>
-                <p>...</p>
+              <img class="d-block w-100" src={dishpit} alt="First slide" />
+              <div style={carouselText} class="carousel-caption d-none d-md-block">
+                <h1>No call. No Show. No Problem.</h1>
+                <p>Essential personnel, when you need it</p>
               </div>
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src={logo} alt="Second slide" />
+              <img class="d-block w-100" src={prep} alt="Second slide" />
               <div class="carousel-caption d-none d-md-block">
-                <h5>...</h5>
-                <p>...</p>
+                <h5>Insure you're properly staffed for peak hours.</h5>
+                <p>Only pay for the hours you need.</p>
               </div>
             </div>
             <div class="carousel-item">
-              <img class="d-block w-100" src={logo} alt="Third slide" />
+              <img class="d-block w-100" src={glasses} alt="Third slide" />
               <div class="carousel-caption d-none d-md-block">
-                <h5>...</h5>
+                <h5>Recruit dishwashers, prepcooks, and other boh staff now!</h5>
                 <p>...</p>
               </div>
             </div>
